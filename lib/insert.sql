fafd6cacc3fb0c642c04a090a857d807ea8bee32
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-12-14 22:26:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender color, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "chill", 1)
+INSERT INTO bears (name, age, gender color, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "chill", 1);
 INSERT INTO bears (name, age, gender color, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender color, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender color, color, temperament, alive) VALUES ()
